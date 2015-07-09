c6f88033844b909be1740aa971ad95b590ba67b9
Raziq <mohideen.syed@rocketmail.com>
2015-07-09 17:02:06 -0400

    Basic leaderboard refresh support

--- app/src/main/java/com/wolfpakapp/wolfpak/leaderboard/LeaderboardActivity.java
+++ app/src/main/java/com/wolfpakapp/wolfpak/leaderboard/LeaderboardActivity.java
@@ -2,6 +2,8 @@ package com.wolfpakapp.wolfpak.leaderboard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
@@ -23,20 +25,58 @@ import java.util.List;
 public class LeaderboardActivity extends Activity {
     private List<LeaderboardListItem> listItems;
     private LeaderboardAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
         listItems = new ArrayList<>();
-        RecyclerView leaderboardRecyclerView = (RecyclerView) findViewById(R.id.leaderboard_recycler_view);
-
         mAdapter = new LeaderboardAdapter(this, listItems);
 
+        RecyclerView leaderboardRecyclerView =
+            (RecyclerView) findViewById(R.id.leaderboard_recycler_view);
         leaderboardRecyclerView.setHasFixedSize(true);
-
         leaderboardRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         leaderboardRecyclerView.setAdapter(mAdapter);
+
+        final SwipeRefreshLayout mLayout =
+            (SwipeRefreshLayout) findViewById(R.id.leaderboard_swipe_refresh_layout);
+        mLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                WolfpakRestClient.get("posts/leaderboard/", null, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                        final JSONArray resArray;
+                        try {
+                            resArray = new JSONArray(new String(bytes));
+                            for (int idx = 0; idx < resArray.length(); idx++) {
+                                JSONObject listItemObject = resArray.getJSONObject(idx);
+
+                                boolean isImage = listItemObject.optBoolean("is_image");
+                                int id = listItemObject.optInt("id");
+                                String handle = listItemObject.optString("handle");
+                                int voteCount = listItemObject.optInt("likes");
+                                String mediaUrl = listItemObject.optString("media_url");
+
+                                listItems.add(new LeaderboardListItem(id, handle, voteCount, mediaUrl, isImage));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        mAdapter.notifyDataSetChanged();
+                        mLayout.setRefreshing(false);
+                    }
+
+                    @Override
+                    public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                        Toast.makeText(LeaderboardActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        mLayout.setRefreshing(false);
+                    }
+                });
+            }
+        });
 
         WolfpakRestClient.get("posts/leaderboard/", null, new AsyncHttpResponseHandler() {
             @Override
